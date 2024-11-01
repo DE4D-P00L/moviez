0071b4a6eeb08acc48a9f253d5823e00a2bd4c57
DE4D-P00L <93002267+DE4D-P00L@users.noreply.github.com>
2024-11-01 11:04:21 +0530

    Removed auth

--- authentication/checkLogin.js
+++ authentication/checkLogin.js
@@ -1,3 +1,3 @@
 const user = localStorage.getItem("user") || "";
 
-if (user && user.length > 0) window.location.href = "../index.html";
+// if (user && user.length > 0) window.location.href = "../index.html";
